338a7a62455d9b129c2a253c1353e034335483ae
hebin123456 <654190921@qq.com>
2017-06-28 20:53:48 +0800

    update
    fixed bugs in windows: some machine can't save the default settings.

--- SUDA_WIFI_win/SUDA_WIFI/MainForm.cs
+++ SUDA_WIFI_win/SUDA_WIFI/MainForm.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
@@ -29,8 +30,18 @@ namespace SUDA_WIFI
 			//
 			InitializeComponent();
 			
-			textBox1.Text = Settings.Default.username.Trim();
-			textBox2.Text = Settings.Default.password.Trim();
+			// textBox1.Text = Settings.Default.username.Trim();
+			// textBox2.Text = Settings.Default.password.Trim();
+			if(File.Exists(@"config.ini")){
+				try{
+					string[] str = File.ReadAllLines(@"config.ini");
+					textBox1.Text = str[0];
+					textBox2.Text = Decrypt(str[1]);
+				}
+				catch{
+					
+				}
+			}
 			
 			string result = getRequest("http://a.suda.edu.cn/index.php/index/init?");
 			if(result.Contains("\"status\":1")){
@@ -53,9 +64,13 @@ namespace SUDA_WIFI
 					MessageBox.Show("登录成功！");
 					button_Login.Text = "下线";
 					flag = 1;
-					Settings.Default.username = textBox1.Text;
-					Settings.Default.password = textBox2.Text;
-					Settings.Default.Save();
+					// Settings.Default.username = textBox1.Text;
+					// Settings.Default.password = textBox2.Text;
+					// Settings.Default.Save();
+					string path = @"config.ini";
+					// 用覆盖的方式写入  
+					string contents = textBox1.Text + "\r\n" + Encrypt(textBox2.Text);
+					File.WriteAllText(path, contents);  
 				}
 				else{
 					int a = result.IndexOf("\"info\":\"");
@@ -147,5 +162,59 @@ namespace SUDA_WIFI
 			Manage manage = new Manage();
 			manage.ShowDialog();
 		}
+		
+		static string encryptKey = "Oyea";    //定义密钥
+		#region 加密字符串  
+        /// <summary> /// 加密字符串   
+        /// </summary>  
+        /// <param name="str">要加密的字符串</param>  
+        /// <returns>加密后的字符串</returns>  
+        static string Encrypt(string str)  
+        {    
+            DESCryptoServiceProvider descsp = new DESCryptoServiceProvider();   //实例化加/解密类对象   
+    
+            byte[] key = Encoding.Unicode.GetBytes(encryptKey); //定义字节数组，用来存储密钥    
+   
+            byte[] data = Encoding.Unicode.GetBytes(str);//定义字节数组，用来存储要加密的字符串  
+     
+            MemoryStream MStream = new MemoryStream(); //实例化内存流对象      
+  
+            //使用内存流实例化加密流对象   
+            CryptoStream CStream = new CryptoStream(MStream, descsp.CreateEncryptor(key, key), CryptoStreamMode.Write);     
+  
+            CStream.Write(data, 0, data.Length);  //向加密流中写入数据      
+  
+            CStream.FlushFinalBlock();              //释放加密流      
+  
+            return Convert.ToBase64String(MStream.ToArray());//返回加密后的字符串  
+        }  
+        #endregion 
+  
+        #region 解密字符串   
+        /// <summary>  
+        /// 解密字符串   
+        /// </summary>  
+        /// <param name="str">要解密的字符串</param>  
+        /// <returns>解密后的字符串</returns>  
+        static string Decrypt(string str)  
+        {      
+            DESCryptoServiceProvider descsp = new DESCryptoServiceProvider();   //实例化加/解密类对象    
+     
+            byte[] key = Encoding.Unicode.GetBytes(encryptKey); //定义字节数组，用来存储密钥    
+    
+            byte[] data = Convert.FromBase64String(str);//定义字节数组，用来存储要解密的字符串  
+      
+            MemoryStream MStream = new MemoryStream(); //实例化内存流对象      
+   
+            //使用内存流实例化解密流对象       
+            CryptoStream CStream = new CryptoStream(MStream, descsp.CreateDecryptor(key, key), CryptoStreamMode.Write);   
+   
+            CStream.Write(data, 0, data.Length);      //向解密流中写入数据     
+  
+            CStream.FlushFinalBlock();               //释放解密流      
+  
+            return Encoding.Unicode.GetString(MStream.ToArray());       //返回解密后的字符串  
+        }  
+        #endregion 
 	}
 }
--- SUDA_WIFI_win/SUDA_WIFI/Manage.cs
+++ SUDA_WIFI_win/SUDA_WIFI/Manage.cs
@@ -29,8 +29,18 @@ namespace SUDA_WIFI
 			//
 			InitializeComponent();
 			
-			textBox1.Text = Settings.Default.username.Trim();
-			textBox2.Text = Settings.Default.password.Trim();
+			// textBox1.Text = Settings.Default.username.Trim();
+			// textBox2.Text = Settings.Default.password.Trim();
+			if(File.Exists(@"config.ini")){
+				try{
+					string[] str = File.ReadAllLines(@"config.ini");
+					textBox1.Text = str[0];
+					textBox2.Text = Decrypt(str[1]);
+				}
+				catch{
+					
+				}
+			}
 			
 			GetValidateImage();
 			//
@@ -265,5 +275,59 @@ namespace SUDA_WIFI
 				}
 			}
 		}
+        
+        static string encryptKey = "Oyea";    //定义密钥
+		#region 加密字符串  
+        /// <summary> /// 加密字符串   
+        /// </summary>  
+        /// <param name="str">要加密的字符串</param>  
+        /// <returns>加密后的字符串</returns>  
+        static string Encrypt(string str)  
+        {    
+            DESCryptoServiceProvider descsp = new DESCryptoServiceProvider();   //实例化加/解密类对象   
+    
+            byte[] key = Encoding.Unicode.GetBytes(encryptKey); //定义字节数组，用来存储密钥    
+   
+            byte[] data = Encoding.Unicode.GetBytes(str);//定义字节数组，用来存储要加密的字符串  
+     
+            MemoryStream MStream = new MemoryStream(); //实例化内存流对象      
+  
+            //使用内存流实例化加密流对象   
+            CryptoStream CStream = new CryptoStream(MStream, descsp.CreateEncryptor(key, key), CryptoStreamMode.Write);     
+  
+            CStream.Write(data, 0, data.Length);  //向加密流中写入数据      
+  
+            CStream.FlushFinalBlock();              //释放加密流      
+  
+            return Convert.ToBase64String(MStream.ToArray());//返回加密后的字符串  
+        }  
+        #endregion 
+  
+        #region 解密字符串   
+        /// <summary>  
+        /// 解密字符串   
+        /// </summary>  
+        /// <param name="str">要解密的字符串</param>  
+        /// <returns>解密后的字符串</returns>  
+        static string Decrypt(string str)  
+        {      
+            DESCryptoServiceProvider descsp = new DESCryptoServiceProvider();   //实例化加/解密类对象    
+     
+            byte[] key = Encoding.Unicode.GetBytes(encryptKey); //定义字节数组，用来存储密钥    
+    
+            byte[] data = Convert.FromBase64String(str);//定义字节数组，用来存储要解密的字符串  
+      
+            MemoryStream MStream = new MemoryStream(); //实例化内存流对象      
+   
+            //使用内存流实例化解密流对象       
+            CryptoStream CStream = new CryptoStream(MStream, descsp.CreateDecryptor(key, key), CryptoStreamMode.Write);   
+   
+            CStream.Write(data, 0, data.Length);      //向解密流中写入数据     
+  
+            CStream.FlushFinalBlock();               //释放解密流      
+  
+            return Encoding.Unicode.GetString(MStream.ToArray());       //返回解密后的字符串  
+        }  
+        #endregion 
 	}
 }
